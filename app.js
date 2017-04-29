9bfae4a9c385d9093975846fedc420c2ea84700a
jellybeanjohnny <mamerige@gmail.com>
2017-04-29 15:20:33 -0400

    Adds schema and model for the JMDict, and adds an example query looking up a word

--- app.js
+++ app.js
@@ -10,6 +10,57 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 const server = http.createServer(app);
 
+var Schema = mongoose.Schema;
+
+mongoose.connect(databaseURL);
+
+var jmdictSchema = new Schema({
+    id : Number, 
+    kanji : [ {
+      common : Boolean, 
+      text : String, 
+      tags : [ String ]
+    } ], 
+    kana : [ {
+      common : Boolean, 
+      text : String, 
+      tags : [ String ], 
+      appliesToKanji : [ String ]
+    } ], 
+    sense : [ {
+      partOfSpeech : [ String ], 
+      appliesToKanji : [ String ], 
+      appliesToKana : [ String ], 
+      related : [ [ String ], [ String ] ], 
+      antonym : [ String ], 
+      field : [ String ], 
+      dialect : [ String ], 
+      misc : [ String ], 
+      info : [ String ], 
+      languageSource : [ String ], 
+      gloss : [ {
+        lang : String, 
+        text : String
+      } ]
+    } ]
+},
+{collection: 'JMDict'}
+);
+
+var JMDict = mongoose.model('JMDict', jmdictSchema);
+
+app.get('/', function (request, response) {
+    response.send('HOME');
+    console.log('Inside home');
+    JMDict.find({$or:[{kanji:{ $elemMatch: {text: "漢数字ゼロ"}}}, {kana:{ $elemMatch: {text: "かんすうじゼロ"}}}]}, function(err, items) {
+        console.log('Hello!');
+        if (err) {
+            console.log(err);
+        }
+        console.log(items);
+    });
+});
+
 server.listen(PORT, function() {
     console.log('Memory-Server listening on port %s', server.address().port);
 });
